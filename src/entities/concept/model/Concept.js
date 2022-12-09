d9141706e3da8b728ae252b0ec610c744ccf49f9
PAtrickSpace <jplsota01@gmail.com>
2022-12-08 20:13:32 -0500

    fix models

--- src/entities/concept/model/Concept.js
+++ src/entities/concept/model/Concept.js
@@ -2,10 +2,10 @@ const { Schema, model } = require("mongoose");
 
 const conceptSchema = new Schema({
   name: { type: String, required: true },
-  amount: { type: String, required: true },
-  currency: { type: String, required: true },
+  amount: { type: Number, required: true },
+  currency: { ref: "Currency", Type: Schema.Types.ObjectId, required: true },
   type: { type: String, required: true }, // activo o pasivo
-  card: { type: String, required: false },
+  card: { ref: "Card", Type: Schema.Types.ObjectId, required: false },
 });
 
 module.exports = model("Concept", conceptSchema);
